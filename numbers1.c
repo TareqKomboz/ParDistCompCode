742e282d155d4d491ece5317c2c7bf0372804547
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-04-06 21:00:23 +0930

    Draft Psedo code Program with comments

---
+++ numbers1.c
@@ -0,0 +1,19 @@
+/*
+Name:           Tareq Abu El Komboz
+StudentID:      A1976365
+Description:    This is Assignement 1 Milestone Program. The objective is to find numbers out of order in a ring topology, and print information about them.
+*/
+
+// How neighbour relationships between tasks are implemented
+
+// Import libraries
+
+// Master Process
+// Slave Process
+// Calculation of left neigbor rank
+// Calculation of right neigbor rank
+// Left check if in order
+// Right check if in order
+// If one check failed
+//      Print out-of-order warning
+// Print Terminiation Indication
