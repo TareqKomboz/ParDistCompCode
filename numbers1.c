264883e79bd057107a87e17ef84f08355755ad54
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-04-06 22:09:50 +0930

    Add MPI start and finish

--- numbers1.c
+++ numbers1.c
@@ -11,16 +11,31 @@ Description:    This is Assignement 1 Milestone Program. The objective is to fin
 #include <stdio.h>
 #include <stdlib.h>
 
-// Master Process
-// Slave Process
-// Calculation of left neigbor rank
-// Calculation of right neigbor rank
-// Left check if in order
-// Right check if in order
-// If one check failed
-//      Print out-of-order warning
-// Print Terminiation Indication
 int main(int argc, char *argv[]) {
-    printf("Hello World");
+    // Initialize variables for each process
+    int rank, size;     // Rank of the process and total number of processes
+    int number;         // The number held by this process
+    int left_number;    // Number received from the left neighbour
+
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Todo:
+    // Master Process
+    // Slave Process
+    // Calculation of left neigbor rank
+    // Calculation of right neigbor rank
+    // Left check if in order
+    // Right check if in order
+    // If one check failed
+    //      Print out-of-order warning
+
+    // Indicate that the process has finished executing.
+    printf("Process %d has now finished.\n", rank);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
     return 0;
 }
